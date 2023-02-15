b7398a48efbbe8ae94af06c90197bf317bcce463
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-15 17:03:22 +0400

    js enter

---
+++ JavaScript/VanillaJs/lesson1-introdction/js/script.js
@@ -0,0 +1,15 @@
+// document.write("Hello external js"); one line
+
+/*
+console.log("Hello1");
+console.log("Hello2");
+console.log("Hello3");
+console.log("Hello4");
+more line
+*/
+
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
---
+++ JavaScript/VanillaJs/lesson2-datatypes/js/script.js
@@ -0,0 +1,46 @@
+
+// Primitive dt
+// string
+// var info = "JavaScript";
+// console.log(info);
+
+// number
+// console.log(5+15);
+
+// boolean
+// true -> 1
+// false -> 0
+// if (false) {
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+// }
+
+// Composite dt
+// array
+// const info = [5,10,'hello',true];
+// console.log(info);
+
+// object
+// const info = {
+//     name:"JS",
+//     pl:true,
+//     age:26
+// }
+// console.log(info.pl);
+
+// Special dt
+// undefined
+// var info;
+// console.log(info);
+// null
+// console.log(null);
+
+
+var title = "JavaScript";
+var age;
+var currentTime = 2023;
+var birthYear = 1997;
+age = currentTime-birthYear;
+
+console.log(title,age);
