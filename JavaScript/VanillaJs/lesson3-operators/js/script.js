196fedf6569ef5c0e7a64a674d089e534e97d8a0
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-18 16:59:49 +0400

    operators

---
+++ JavaScript/VanillaJs/lesson3-operators/js/script.js
@@ -0,0 +1,116 @@
+// Assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+// Arithmetic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+
+// Comparative operators
+// ==
+// ===
+// <
+// <=
+// >
+// >=
+// !=
+// !==
+
+// var infoOne = "C";
+// var infoTwo = "b";
+
+// if (infoOne < infoTwo) {
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+// }
+
+
+// logical operators
+// && - AND
+// || - OR
+// ! - not
+
+// var user  = {
+//     pass:"info2023",
+//     email:"info@example.com"
+// }
+
+// var emailInput = "a";
+// var passInput = "a";
+// if (!emailInput || !passInput) {
+//     console.log('fill input');
+// }else{
+//     if (emailInput === user.email && passInput === user.pass) {
+//         console.log('login successfull');
+//     }else{
+//         console.log('pass or email is wrong!');
+//     }
+// }
+
+
+// string operators
+// +
+// +=
+// var a = "JavaScript ";
+// a+="Hello"
+// console.log(a);
+
+
+// Special Operators
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,      (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5, b = 10;
+// console.log(a,b);
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var a = "JavaScript\"s book";
+// console.log(a);
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+    // var a = 10;
+    // console.log(typeof a);
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a  = 5;
+// console.log(void(a));
+
+// delete       (objectin dəyərini silmək üçündür)
+// var a = {
+//     name:'js'
+// }
+// delete a.name
+// console.log(a.name);
+// new          (yeni dəyər yaratməq üçündür)
+
+// this         (mövcud dəyəri işarələmək üçündür)
+
+
+// in           (dəyişkənin daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
