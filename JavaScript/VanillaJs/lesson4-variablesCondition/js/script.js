eb0ad95fa510e0a58719a7f4f03bc09431e9747f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-22 17:35:16 +0400

    if

---
+++ JavaScript/VanillaJs/lesson4-variablesCondition/js/script.js
@@ -0,0 +1,111 @@
+// var $1_Info_Resul$t = 5;
+//  $1_Info_Resul$t = 15;
+// console.log($1_Info_Resul$t);
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərflərə həssasdır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// let      -> local
+// const    -> local    ->      constant
+// var      -> global   ->      variable
+
+// global
+
+// if (true) {
+//   // local global
+
+//   if (true) {
+//     // local global
+//     if (true) {
+//       // local
+//       let a = 5;
+//     }
+
+// console.log(a);
+
+//   }
+// }
+
+
+// console.log(a);
+// var a = 5;
+
+
+
+
+// const  a = 5;
+// console.log(a);
+
+
+// if - eger
+// else - deyilse
+// else if - deyilse eger 
+
+
+// let a = 110;
+// let b = 1011;
+// if (a>b) {
+//     console.log('this is true');
+// }else if(a==b){
+//     console.log('equale');
+// }
+// else{
+//     console.log('this is false');
+// }
+
+// const myTime = 6;
+
+
+// if (myTime > 5 && myTime <=12) {
+//     console.log('good morning');
+//     if (myTime == 6) {
+//         console.log('get up');
+//     }else if(myTime == 7){
+//         console.log('have a breakfast');
+//     }
+// }else if(myTime > 12 && myTime <=18){
+//             console.log('good afternoon');
+// }else if(myTime > 18 && myTime <=22){
+//     console.log('good evening');
+// }else if(myTime > 22 && myTime <=23){
+//     console.log('good night');
+// }else if(myTime == 0){
+//     console.log('go to sleep ');
+// }
+// else{
+//     console.log('no time');
+
+// }
+
+// var employeeWorkTime = 5;
+
+
+
+// const product1 = "milk,bread,butter,cheese"; =10
+// const product2 = "meal,holland cheese"; =20
+// const product3 = "milk,bread,better,cheese";
+// const product4 = "milk,bread,better,cheese";
