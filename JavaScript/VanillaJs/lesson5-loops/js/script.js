c51820723e7d24ea7e61ac8cef6327f7368efab5
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-25 16:44:38 +0400

    array

---
+++ JavaScript/VanillaJs/lesson5-loops/js/script.js
@@ -0,0 +1,75 @@
+
+
+
+// let info = 0;
+// while (info > 10) {
+//     info++;
+//     console.log(info);
+// }
+
+
+// let info = 0;
+// do{
+//     console.log(info);
+//     info++;
+// }while (info>10)
+
+
+// for(let info = 0; info < 10; info++){
+//     console.log(info);
+// }
+
+
+// const infoArr = ['Hello',true,5,undefined,null,{name:'test'},['data']];
+// console.log(typeof infoArr);
+
+// console.log();
+
+// const infoProduct = ['table','chair','sofa','bed','table','chair','sofa','bed']
+// for(let count=0; count < infoProduct.length; count++ ){
+
+//     console.log(infoProduct[count]);
+// }
+// console.log(infoProduct.length);
+
+// const infoProductOne = ['table','chair','sofa','bed','table','chair','sofa','bed',];
+// const infoProductTwo = ['table','chair','sofa','bed',];
+// console.log(infoProductTwo.length);
+// const infoProduct = ['table','chair','sofa','bed',];
+// infoProduct[2] = 'new value'
+// let a;
+// a = 5;
+// console.log(a);
+
+// const infoProduct = [];
+// infoProduct[0]='table';
+// infoProduct[1]='chair';
+
+// console.log(infoProduct);
+
+// let info;
+// let switchOne = false;
+// let switchTwo = true;
+// let switchThree = false;
+// if (switchOne) {
+//     info = 10;
+//     for(let c = 0; c < info; c++){
+//         console.log("switchOne"+c);
+//     }
+// }
+// if (switchTwo) {
+//      info = 20;
+//     for(let c = 0; c < info; c++){
+//         console.log("switchTwo"+c);
+//     }
+// }
+// if (switchThree) {
+//     info = 30;
+//     for(let c = 0; c < info; c++){
+//         console.log("switchThree"+c);
+//     }
+// }
+
+
+
+
