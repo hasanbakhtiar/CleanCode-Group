beec5bf95d999863ec908b8e0260e0da66a4f929
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-01 17:15:28 +0400

    js array

---
+++ JavaScript/VanillaJs/lesson6-arrayObject/js/script.js
@@ -0,0 +1,123 @@
+// const info = ['table','chair','sofa'];
+// console.log(info);
+
+// for in
+// for(let count in info){
+//     console.log(info[count]);
+// }
+
+// for of
+// for (let data of info){
+//     console.log(data);
+// }
+
+// let info = ['table','chair','sofa'];
+// info.pop();
+// info.shift();
+// info.push('new value')
+// info.unshift('new value')
+// console.log(info);
+
+// function myFunc (){
+//    info = ['table','chair']
+// }
+// myFunc();
+// console.log(info);
+
+// const car = [
+//   {
+//     title: "BMW",
+//     model: "X5",
+//     color: ["yellow", "orange", "black"],
+//     newCar: false,
+//     year: 2021,
+//     // country:"England"
+//   },
+//   {
+//     title: "Mercedes",
+//     model: "s500",
+//     color: "black",
+//     newCar: true,
+//     year: 2023,
+//   },
+//   {
+//     title: "Audi",
+//     model: "a5",
+//     color: "blue",
+//     newCar: false,
+//     year: 1990,
+//   },
+//   {
+//     title: "Lada",
+//     model: "06",
+//     color: "orange",
+//     newCar: true,
+//     year: 1981,
+//   },
+// ];
+
+// for( let i in car){
+//     if (!car[i].newCar) {
+//         // console.log('new car list');
+//         console.log(car[i]);
+//     }
+//     // else{
+//     //     console.log('old car list');
+//     //     console.log(car[i].title);
+
+//     // }
+// }
+
+// for (let i in car) {
+//   if (car[i].newCar) {
+//     console.log(
+//       `Brand:${car[i].title} | Model: ${car[i].model} | Color: ${car[i].color}`
+//     );
+//   }
+// }const car = [
+//   {
+//     title: "BMW",
+//     model: "X5",
+//     color: ["yellow", "orange", "black"],
+//     newCar: false,
+//     year: 2021,
+//     // country:"England"
+//   },
+//   {
+//     title: "Mercedes",
+//     model: "s500",
+//     color: "black",
+//     newCar: true,
+//     year: 2023,
+//   },
+//   {
+//     title: "Audi",
+//     model: "a5",
+//     color: "blue",
+//     newCar: false,
+//     year: 1990,
+//   },
+//   {
+//     title: "Lada",
+//     model: "06",
+//     color: "orange",
+//     newCar: true,
+//     year: 1981,
+//   },
+// ];
+
+
+
+// const arr1 = ["a", "b"];
+// const arr2 = ["c", "d", "e"];
+// const total = arr1.concat(arr2);
+// console.log(total);
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(...fruits);
+// let text = fruits.join(" | ");
+
+// console.log(text);
+// console.log(fruits.sort());
+// console.log(fruits.reverse());
