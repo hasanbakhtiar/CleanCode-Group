a5852d3d1c299eb55364d1565fbd453354e2bbbb
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-04 17:13:07 +0400

    function

---
+++ JavaScript/VanillaJs/lesson7-functions/js/script.js
@@ -0,0 +1,79 @@
+// function name(params) {
+//         // codes 
+// }
+
+
+
+// functrion declaration
+// info();
+// function info (){
+//          console.log("hello function");
+// }
+
+
+// function expression
+// myInfo()
+// const myInfo = function(){
+//         console.log('hello expression');
+// }
+
+// (function() {
+//         console.log('Hello');
+// })()
+
+
+// function car (km,hour){
+//         console.log(km/hour+"km/h");
+// }
+
+// car(100,4);
+
+
+
+
+// immediately invoked function expression (IIFE)
+// (function(km,hour){
+//         console.log(km/hour+"km/h");
+// })(100,10)
+
+
+// arrow function
+
+// function(){} - normal
+// ()=>{} - arrow
+
+// const car =(km=100,hour=5)=>{
+//         console.log(km/hour+"km/h");
+// }
+
+// car(300,9);
+
+
+
+// const comingData = ()=>{
+//         return "Hello";
+// }
+// console.log(comingData());
+
+
+// const infoOne =(comingdata)=>{
+//         return comingdata
+// }
+
+// const infoTwo=(dataOne,dataTwo)=>{
+
+//         const total = dataOne+dataTwo;
+
+//         if (typeof total === 'number') {
+//                 console.log(infoOne(total));
+//         }
+// }
+// infoTwo(10,20)
+
+const mycategory = prompt();
+
+
+function data(category){
+
+}
+data(mycategory)
