8ebd9204dc1361bb0768cfeffe475fdab8b3a2ce
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-04-05 17:36:45 +0400

    api

--- JavaScript/VanillaJs/lesson12-DOMpart4/js/script.js
+++ JavaScript/VanillaJs/lesson12-DOMpart4/js/script.js
@@ -1,11 +1,11 @@
 // console.log(fetch());
 fetch('http://127.0.0.1:5500/data/data.json')
 .then(res=>res.json())
-.then((data)=>{
+.then((a)=>{
   
 let li ="";
-  console.log(data);
-data.map(item=>{
+  console.log(a);
+a.slice(5,10).map(item=>{
   li+= `<li><img width="100" src="${item.image}">${item.title}</li>`;
 })
 
