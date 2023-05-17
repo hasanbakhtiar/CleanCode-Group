5697d229acf5cda1af6c82923bc9c14c4f7db74e
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-17 17:28:41 +0400

    api

--- JavaScript/ReactJS/lesson6-api/src/App.jsx
+++ JavaScript/ReactJS/lesson6-api/src/App.jsx
@@ -3,21 +3,21 @@ import SingleCard from './components/SingleCard'
 import axios from 'axios';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      data:[]
+      data: []
     }
-  
+
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
 
     axios.get("https://rickandmortyapi.com/api/character")
-    .then(res=>this.setState({data:res.data.results}))
-    
-    
-    
+      .then(res => this.setState({ data: res.data.results }))
+
+
+
 
     // const fetchApi = async()=>{
     //     const items = await fetch('https://rickandmortyapi.com/api/character')
@@ -25,25 +25,28 @@ class App extends Component {
     //     this.setState({data:mainItems.results})
     // }
     // fetchApi();
-    
+
     // fetch("https://rickandmortyapi.com/api/character")
     // .then(res=>res.json())
     // .then(items=>this.setState({data:items.results}))
   }
-  
-  
+
+
   render() {
     return (
       <div className='container'>
         <h1 className='text-center my-5'>characters list</h1>
+        <button className='btn btn-danger mb-3'
+          onClick={() => { this.setState({ data: [] }) }}
+        >delete all</button>
         <div className="row g-5">
-      
-        {this.state.data.map(item=>(
-          <SingleCard key={item.id}
-          alldata = {item}
-          />
-        ))}
-   
+
+          {this.state.data.length === 0 ? <h1>all data deleted</h1> : this.state.data.map(item => (
+            <SingleCard key={item.id}
+              alldata={item}
+            />
+          ))}
+
         </div>
       </div>
     )
