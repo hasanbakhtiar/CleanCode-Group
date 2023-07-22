bb9a4cb0505c591f225cabb8c3750ffe27eb84b6
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-22 12:19:22 +0400

    ts enter

---
+++ JavaScript/TypeScript/lesson1-introduction/ts/app.js
@@ -0,0 +1,17 @@
+// var info:string = "My TS text";
+// console.log(info);
+// var product:string[] = ['phone','laptop',"PC"]
+// var product:Array<string> = ['phone','laptop',"PC"]
+// product.push(5);
+// console.log(product);
+// type productType ={
+//     title:string,
+//     price:number,
+//     stock:boolean
+// }
+// var product:productType = {
+//     title:"MSI Sword",
+//     price:1000,
+//     stock:true
+// }
+// console.log(product);
