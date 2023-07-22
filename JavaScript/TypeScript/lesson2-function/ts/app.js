62e522d991230f81fb3b046603664890aa55102f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-22 12:08:13 +0400

    ts enter

---
+++ JavaScript/TypeScript/lesson2-function/ts/app.js
@@ -0,0 +1,12 @@
+// function info(paramsOne:string,paramsTwo:boolean):void{
+//     if (paramsTwo) {
+//         console.log(paramsOne);
+//     }else{
+//         console.log('wrong');
+//     }
+// }
+// info("Hello TS",true);
+// const info:any = function(){
+//     return 'test';
+// }
+// console.log(info);
