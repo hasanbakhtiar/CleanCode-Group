33019e19fc90747184f8de24f44cdb918059f3e7
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-22 12:51:07 +0400

    ts enter

---
+++ JavaScript/TypeScript/lesson4-DOM/ts/app.js
@@ -0,0 +1,4 @@
+// var text:any = document.querySelector<HTMLHeadingElement>('h1');
+// text.innerHTML = 'test'
+var myText = document.querySelector('ol');
+myText.innerHTML = 'text';
---
+++ JavaScript/TypeScript/lesson4-DOM/ts/app.ts
@@ -0,0 +1,6 @@
+// var text:any = document.querySelector<HTMLHeadingElement>('h1');
+// text.innerHTML = 'test'
+
+var myText:any = document.querySelector('ol') as HTMLOListElement;
+
+myText.innerHTML  = 'text'
